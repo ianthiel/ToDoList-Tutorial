f50c15233440984d95a94103708e3431bbe24d7d
Ian Thiel <ian@Ians-MacBook-Pro.local>
2015-04-20 21:20:12 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ian Thiel on 4/20/15.
+//  Copyright (c) 2015 Ian Thiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
